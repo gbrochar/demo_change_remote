3ac007565a6d61d71af9aa68e1d26fc4c8969ebc
Gitea <gitea@fake.local>
2023-09-20 12:17:51 +0000

    new feature a merger

--- annonce30.py
+++ annonce30.py
@@ -1,2 +1,2 @@
 def annonce():
-    return "Cette formation git est vraiment enrichissante, j'adore le contrôle de version !!!"
+    return "Cette formavraiment enrichissante, j'adore le contrôle de version !!!"
